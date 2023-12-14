d7b1556de399c740cbf05206846f793798e12960
Ameer <ameerapex496@gmail.com>
2023-12-14 09:40:51 +0100

    added comments , selected elements needed

--- src/script.js
+++ src/script.js
@@ -1,1 +1,11 @@
-console.log("hallo world");
+// selecting elements
+var addTodoBtn = document.getElementsByClassName("add-Todo-Btn")[0];
+var textInput = document.getElementsByClassName("text-Input")[0];
+var todosCardsWrap = document.getElementsByClassName("todos-Cards-Wrap")[0];
+// global variables
+// functions
+var addTodo = function () {
+    console.log("hallo world");
+};
+// event lisnters
+addTodoBtn.addEventListener("click", addTodo);
--- src/script.ts
+++ src/script.ts
@@ -1,1 +1,14 @@
-console.log("hallo world");
+// selecting elements
+const addTodoBtn = document.getElementsByClassName("add-Todo-Btn")[0];
+const textInput = document.getElementsByClassName("text-Input")[0];
+const todosCardsWrap = document.getElementsByClassName("todos-Cards-Wrap")[0];
+
+// global variables
+
+// functions
+const addTodo = () => {
+  console.log("hallo world");
+};
+
+// event lisnters
+addTodoBtn.addEventListener("click", addTodo);
